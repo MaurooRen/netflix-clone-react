7e60311106635faf3a6681320b3f04f9cacfd10d
MaurooRen <maurooren@gmail.com>
2021-02-14 19:56:42 -0500

    working over custom axios for Row component

--- src/components/Row.js
+++ src/components/Row.js
@@ -9,7 +9,21 @@ const Row = ({ title, fetchUrl }) => {
     useEffect(() => {
         //We want this code to run once the Row component is loaded
         //Make a request to TMDM API
-    }, [])
+
+        //Async Call
+        //We do this because we are making a call to a thirt party service,
+        //so it may take 0 or 1s maybe more
+        async function fetchData() {
+            //When you make this 'request' WAIT for the answear to comeback
+            //and the DO something
+            const request = await axios.get(fetchUrl)
+            setMovies(request.data)
+        }
+        fetchData()
+    }, [fetchUrl])
+    //Whenever useEffect its depending of an external variable
+    //(in this case fetchUrl) WE HAVE TO!!! use it use in []
+    //because we are depending of that variable
 
     return (
         <div>
