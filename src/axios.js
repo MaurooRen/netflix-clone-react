9d63daeaea63bc086178d4adc4f7861749e23b85
MaurooRen <maurooren@gmail.com>
2021-02-14 19:21:01 -0500

    adding comments to axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 
-
+// base url to make requests to the movie database
 const instance = axios.create({
     baseURL: 'https://api.themoviedb.org/3'
 })
+
 // the instance above allow to do something like this
 // instance.get('/foo-bar');
-// so instance its actually the inicial part of the url
+// https://api.themoviedb.org/3/foo-bar
+
+// So instance its actually the inicial part of the url
 // we use this when we have diferent end point comming
 // from the same API
 
