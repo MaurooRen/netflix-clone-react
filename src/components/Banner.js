da8e75bb14726e51cfa0e9e61a1e6ae41ffe4ed3
MaurooRen <maurooren@gmail.com>
2021-02-15 00:52:07 -0500

    axios into Banner component to generate random data

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from '../consts/axios'
+import requests from '../consts/requests'
+
 
 const Banner = () => {
+
+    const [movie, setMovie] = useState([])
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals)
+            // Here we are chosing one rantom element from the array
+            // from 0 to the array.length - 1
+            setMovie(request.data.results[
+                Math.floor(Math.random() * request.data.results.length - 1)
+            ])
+        }
+        fetchData()
+    }, [])
+
+    console.log(movie)
+
     return (
-        <header> {/* background image */}
+        <header className="banner">
+            <div className="banner_contents">
 
-            {/* title */}
-
-            {/* div > 2 buttons */}
-
-            {/* description */}
-
+            </div>
         </header>
     )
 }
